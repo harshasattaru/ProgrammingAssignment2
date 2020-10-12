ddbbc0589b50167b62b8775bb64986f6b0b3dc0b
sattar96 <sattar96_soe@jnu.ac.in>
2020-10-12 14:54:44 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,25 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(a,b,c,d,e,f,g,h,i) {
+a1<-c(a,b,c)
+a2<-c(d,e,f)
+a3<-c(g,h,i)
+A<-rbind(a1,a2,a3)
+return(A)
+##it takes the values for the matrix
 
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(a, ...) {
+        
+x<-CacheMatrix(a,b,c,d,e,f,g,h,i)
+
+k<-solve(x) ## Return a matrix that is the inverse of 'x'
+
+k
+
 }
